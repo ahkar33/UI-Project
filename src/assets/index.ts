11e6c3310e7fb9e8b00a032719d9717dd7bc3a32
ahkar33 <135191170+ahkartoemaw-lastoenjoy@users.noreply.github.com>
2023-11-17 12:25:12 +0630

    initial commit

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import OPENSEA_ICON from "./discord.svg";
+import REACT_ICON from "./react.svg";
+import TWITTER_ICON from "./twitter.svg";
+import WALKING_BOY from "./walking_boy.gif";
+
+export { OPENSEA_ICON, REACT_ICON, TWITTER_ICON, WALKING_BOY };
